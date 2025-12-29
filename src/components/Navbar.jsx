e4e4b6941b9dc839223382851e7539aacfa8179b
Jakob Szarkowicz (Mac) <admin@szark.org>
2025-12-29 12:57:53 +0100

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "#constants";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">Jakob's MacFolio</p>
+
+                <ul>
+                    {navLinks.map((item) => (
+                        <li key={item.id}>
+                            <p>{item.name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map((icon) => (
+                        <li key={icon.id}>
+                            <img src={icon.img} alt={`icon-${icon.id}`} />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+            </div>
+        </nav>
+    );
+};
+  
+export default Navbar;
